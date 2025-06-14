13fa25b36e01df1e370b81a568456c70d0d4db77
lunarfractal <xxs239239@gmail.com>
2025-06-14 18:07:54 +0530

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    private static int numDigits(int num) {
-        return (int) Math.floor(Math.log10(Math.abs(num))) + 1;
-    }
-    
-    public static void main(String[] args) {
-        System.out.println(numDigits(67757));
-    }
-}
